a774aed9bf970208d69b77de701e715449c37f97
Rohan Agrawal <send2arohan@gmail.com>
2019-01-04 22:46:42 -0800

    make output terminate with cycle count

--- src/main.cpp
+++ src/main.cpp
@@ -135,7 +135,8 @@ uint32_t program[10] = {
 	            0b00000000111100000000010110010011, // set x11 to 15
 	            0b00000000000100000000010100010011, // set x10 to 0
 	            0b00000000000101010000010100010011, // ADDI x10, x10, 1
-	            0b11111110101001011100110011100011, // BLT x11, x10, -8
+	            //0b11111110101001011100110011100011, // BLT x11, x10, -8
+	            0b00000000101001011100010001100011, // BLT x11, x10, +8
                     0b11111111100111111111000001101111  // JAL x0, -8
                   };
 
@@ -145,6 +146,7 @@ int END_PROGRAM = 5;
 
 int main(int argc, char* argv[]) {
 	uint32_t program_counter = 0;
+	uint32_t cycle = 1;
 
 	while (program_counter < END_PROGRAM) {
 		const auto raw_inst = program[program_counter];
@@ -174,8 +176,11 @@ int main(int argc, char* argv[]) {
 		if (!pc_set)
 			program_counter++;
 		printf("PC:%d, x10: %d, x11: %d, x12: %d \n", program_counter, registers[10], registers[11], registers[12]);
-		usleep(500000); // sleep for 500,000 microseconds (0.5 seconds)
+		//usleep(500000); // sleep for 500,000 microseconds (0.5 seconds)
+		cycle++;
 	}
+
+	printf("\n\n\nRan for %d cycles\n", cycle);
 }
 
 
