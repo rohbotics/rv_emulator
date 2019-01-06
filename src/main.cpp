dc5b96caa2a3b340f4e3d9f4a47bb9be0038c081
Rohan Agrawal <send2arohan@gmail.com>
2019-01-05 18:32:17 -0800

    format using clang format and editorconfig

--- src/main.cpp
+++ src/main.cpp
@@ -6,181 +6,178 @@
 #include "decoding.h"
 
 void print_instruction(uint32_t raw_inst) {
-	auto inst = decode_instruction(raw_inst);
+    auto inst = decode_instruction(raw_inst);
 
-	switch (inst.operation) {
-		case Operations::LUI:
-			printf("LUI x%d ← #%d\n", inst.rd, inst.imm);
-			break;
-		case Operations::AUIPC:
-			printf("AUIPC x%d ← #(pc + %d)\n", inst.rd, inst.imm);
-			break;
-		case Operations::JAL: 
-			printf("JAL %d, %d\n", inst.rd, inst.simm);
-			break;
-		case Operations::JALR: 
-			printf("JALR x%d, x%d, #%d\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::BEQ:
-			printf("BEQ x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::BNE:
-			printf("BNE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::BLT:
-			printf("BLT x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::BGE:
-			printf("BGE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::BLTU:
-			printf("BLTU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::BGEU:
-			printf("BGEU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-			break;
-		case Operations::LB:
-			printf("LB x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::LH:
-			printf("LH x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::LW:
-			printf("LW x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::LBU:
-			printf("LBU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::LHU:
-			printf("LHU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::SB:
-			printf("SB [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-			break;
-		case Operations::SH:
-			printf("SH [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-			break;
-		case Operations::SW:
-			printf("SW [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-			break;
-		case Operations::ADDI:
-			printf("ADDI x%d ← x%d + #%d\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::SLTI:
-			printf("SLTI x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.simm);
-			break;
-		case Operations::SLTIU:
-			printf("SLTIU x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.imm);
-			break;
-		case Operations::XORI:
-			printf("XORI x%d ← x%d ^ #%d\n", inst.rd, inst.rs1, inst.imm);
-			break;
-		case Operations::ORI:
-			printf("ORI x%d ← x%d | #%d\n", inst.rd, inst.rs1, inst.imm);
-			break;
-		case Operations::ANDI:
-			printf("ANDI x%d ← x%d & #%d\n", inst.rd, inst.rs1, inst.imm);
-			break;
-		case Operations::SLLI:
-			printf("SLLI\n");
-			break;
-		case Operations::SRLI:
-			printf("SRLI\n");
-			break;
-		case Operations::SRAI:
-			printf("SRAI\n");
-			break;
-		case Operations::ADD:
-			printf("ADD x%d ← x%d + x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SUB:
-			printf("SUB x%d ← x%d - x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SLL:
-			printf("SLL x%d ← x%d << x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SLT:
-			printf("SLT x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SLTU:
-			printf("SLTU x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::XOR:
-			printf("XOR x%d ← x%d ^ x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SRL:
-			printf("SRL x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::SRA:
-			printf("SRA x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::OR:
-			printf("OR x%d ← x%d | x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::AND:
-			printf("AND x%d ← x%d & x%d\n", inst.rd, inst.rs1, inst.rs2);
-			break;
-		case Operations::FENCE:
-			printf("FENCE\n");
-			break;
-		case Operations::FENCEI:
-			printf("FENCEI\n");
-			break;
-	}
+    switch (inst.operation) {
+        case Operations::LUI:
+            printf("LUI x%d ← #%d\n", inst.rd, inst.imm);
+            break;
+        case Operations::AUIPC:
+            printf("AUIPC x%d ← #(pc + %d)\n", inst.rd, inst.imm);
+            break;
+        case Operations::JAL:
+            printf("JAL %d, %d\n", inst.rd, inst.simm);
+            break;
+        case Operations::JALR:
+            printf("JALR x%d, x%d, #%d\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::BEQ:
+            printf("BEQ x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::BNE:
+            printf("BNE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::BLT:
+            printf("BLT x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::BGE:
+            printf("BGE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::BLTU:
+            printf("BLTU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::BGEU:
+            printf("BGEU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
+            break;
+        case Operations::LB:
+            printf("LB x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::LH:
+            printf("LH x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::LW:
+            printf("LW x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::LBU:
+            printf("LBU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::LHU:
+            printf("LHU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::SB:
+            printf("SB [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
+            break;
+        case Operations::SH:
+            printf("SH [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
+            break;
+        case Operations::SW:
+            printf("SW [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
+            break;
+        case Operations::ADDI:
+            printf("ADDI x%d ← x%d + #%d\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::SLTI:
+            printf("SLTI x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.simm);
+            break;
+        case Operations::SLTIU:
+            printf("SLTIU x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.imm);
+            break;
+        case Operations::XORI:
+            printf("XORI x%d ← x%d ^ #%d\n", inst.rd, inst.rs1, inst.imm);
+            break;
+        case Operations::ORI:
+            printf("ORI x%d ← x%d | #%d\n", inst.rd, inst.rs1, inst.imm);
+            break;
+        case Operations::ANDI:
+            printf("ANDI x%d ← x%d & #%d\n", inst.rd, inst.rs1, inst.imm);
+            break;
+        case Operations::SLLI:
+            printf("SLLI\n");
+            break;
+        case Operations::SRLI:
+            printf("SRLI\n");
+            break;
+        case Operations::SRAI:
+            printf("SRAI\n");
+            break;
+        case Operations::ADD:
+            printf("ADD x%d ← x%d + x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SUB:
+            printf("SUB x%d ← x%d - x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SLL:
+            printf("SLL x%d ← x%d << x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SLT:
+            printf("SLT x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SLTU:
+            printf("SLTU x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::XOR:
+            printf("XOR x%d ← x%d ^ x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SRL:
+            printf("SRL x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::SRA:
+            printf("SRA x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::OR:
+            printf("OR x%d ← x%d | x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::AND:
+            printf("AND x%d ← x%d & x%d\n", inst.rd, inst.rs1, inst.rs2);
+            break;
+        case Operations::FENCE:
+            printf("FENCE\n");
+            break;
+        case Operations::FENCEI:
+            printf("FENCEI\n");
+            break;
+    }
 }
-
 
 int32_t registers[32] = {};
 uint32_t program[10] = {
-	            0b00000000111100000000010110010011, // set x11 to 15
-	            0b00000000000100000000010100010011, // set x10 to 0
-	            0b00000000000101010000010100010011, // ADDI x10, x10, 1
-	            //0b11111110101001011100110011100011, // BLT x11, x10, -8
-	            0b00000000101001011100010001100011, // BLT x11, x10, +8
-                    0b11111111100111111111000001101111  // JAL x0, -8
-                  };
+    0b00000000111100000000010110010011,  // set x11 to 15
+    0b00000000000100000000010100010011,  // set x10 to 0
+    0b00000000000101010000010100010011,  // ADDI x10, x10, 1
+    // 0b11111110101001011100110011100011, // BLT x11, x10, -8
+    0b00000000101001011100010001100011,  // BLT x11, x10, +8
+    0b11111111100111111111000001101111   // JAL x0, -8
+};
 
 unsigned int END_PROGRAM = 5;
 
 int main() {
-	uint32_t program_counter = 0;
-	uint32_t cycle = 1;
+    uint32_t program_counter = 0;
+    uint32_t cycle = 1;
 
-	while (program_counter < END_PROGRAM) {
-		const auto raw_inst = program[program_counter];
-		printf("-----------------------------------------\n");
-		print_instruction(raw_inst);
-		const auto inst = decode_instruction(raw_inst);
+    while (program_counter < END_PROGRAM) {
+        const auto raw_inst = program[program_counter];
+        printf("-----------------------------------------\n");
+        print_instruction(raw_inst);
+        const auto inst = decode_instruction(raw_inst);
 
-		bool pc_set = false;
-		switch (inst.operation) {
-			case Operations::ADDI:
-				if (inst.rd != 0)
-					registers[inst.rd] = registers[inst.rs1] + inst.simm;
-				break;
-			case Operations::JAL:
-				if (inst.rd != 0)
-					registers[inst.rd] = program_counter + 1;
-				program_counter += (inst.simm/4);
-				pc_set= true;
-				break;
-			case Operations::BLT:
-				if (registers[inst.rs1] < registers[inst.rs2]) {
-					program_counter += (inst.simm/4);
-					pc_set= true;
-				}
-				break;
-			default:
-				throw std::runtime_error("unhandled instruction");
-		}
-		if (!pc_set)
-			program_counter++;
-		printf("PC:%d, x10: %d, x11: %d, x12: %d \n", program_counter, registers[10], registers[11], registers[12]);
-		//usleep(500000); // sleep for 500,000 microseconds (0.5 seconds)
-		cycle++;
-	}
+        bool pc_set = false;
+        switch (inst.operation) {
+            case Operations::ADDI:
+                if (inst.rd != 0)
+                    registers[inst.rd] = registers[inst.rs1] + inst.simm;
+                break;
+            case Operations::JAL:
+                if (inst.rd != 0)
+                    registers[inst.rd] = program_counter + 1;
+                program_counter += (inst.simm / 4);
+                pc_set = true;
+                break;
+            case Operations::BLT:
+                if (registers[inst.rs1] < registers[inst.rs2]) {
+                    program_counter += (inst.simm / 4);
+                    pc_set = true;
+                }
+                break;
+            default:
+                throw std::runtime_error("unhandled instruction");
+        }
+        if (!pc_set)
+            program_counter++;
+        printf("PC:%d, x10: %d, x11: %d, x12: %d \n", program_counter, registers[10], registers[11], registers[12]);
+        // usleep(500000); // sleep for 500,000 microseconds (0.5 seconds)
+        cycle++;
+    }
 
-	printf("\n\n\nRan for %d cycles\n", cycle);
+    printf("\n\n\nRan for %d cycles\n", cycle);
 }
-
-
--- tests/decoder_test.cpp
+++ tests/decoder_test.cpp
@@ -4,117 +4,117 @@
 #include "test_instructions.h"
 
 TEST_CASE("Operation Decoding", "[decoding]") {
-	REQUIRE(decode_instruction(ilui).operation == Operations::LUI);
-	REQUIRE(decode_instruction(iauipc).operation == Operations::AUIPC);
-	REQUIRE(decode_instruction(ijal).operation == Operations::JAL);
-	REQUIRE(decode_instruction(ijalr).operation == Operations::JALR);
-	REQUIRE(decode_instruction(ibeq).operation == Operations::BEQ);
-	REQUIRE(decode_instruction(ibne).operation == Operations::BNE);
-	REQUIRE(decode_instruction(iblt).operation == Operations::BLT);
-	REQUIRE(decode_instruction(ibge).operation == Operations::BGE);
-	REQUIRE(decode_instruction(ibltu).operation == Operations::BLTU);
-	REQUIRE(decode_instruction(ibgeu).operation == Operations::BGEU);
-	REQUIRE(decode_instruction(ilb).operation == Operations::LB);
-	REQUIRE(decode_instruction(ilh).operation == Operations::LH);
-	REQUIRE(decode_instruction(ilw).operation == Operations::LW);
-	REQUIRE(decode_instruction(ilbu).operation == Operations::LBU);
-	REQUIRE(decode_instruction(ilhu).operation == Operations::LHU);
-	REQUIRE(decode_instruction(isb).operation == Operations::SB);
-	REQUIRE(decode_instruction(ish).operation == Operations::SH);
-	REQUIRE(decode_instruction(isw).operation == Operations::SW);
-	REQUIRE(decode_instruction(iaddi).operation == Operations::ADDI);
-	REQUIRE(decode_instruction(islti).operation == Operations::SLTI);
-	REQUIRE(decode_instruction(isltiu).operation == Operations::SLTIU);
-	REQUIRE(decode_instruction(ixori).operation == Operations::XORI);
-	REQUIRE(decode_instruction(iori).operation == Operations::ORI);
-	REQUIRE(decode_instruction(iandi).operation == Operations::ANDI);
-	REQUIRE(decode_instruction(islli).operation == Operations::SLLI);
-	REQUIRE(decode_instruction(isrli).operation == Operations::SRLI);
-	REQUIRE(decode_instruction(israi).operation == Operations::SRAI);
-	REQUIRE(decode_instruction(iadd).operation == Operations::ADD);
-	REQUIRE(decode_instruction(isub).operation == Operations::SUB);
-	REQUIRE(decode_instruction(isll).operation == Operations::SLL);
-	REQUIRE(decode_instruction(islt).operation == Operations::SLT);
-	REQUIRE(decode_instruction(isltu).operation == Operations::SLTU);
-	REQUIRE(decode_instruction(ixor).operation == Operations::XOR);
-	REQUIRE(decode_instruction(isrl).operation == Operations::SRL);
-	REQUIRE(decode_instruction(isra).operation == Operations::SRA);
-	REQUIRE(decode_instruction(ior).operation == Operations::OR);
-	REQUIRE(decode_instruction(iand).operation == Operations::AND);
-	REQUIRE(decode_instruction(ifence).operation == Operations::FENCE);
-	REQUIRE(decode_instruction(ifencei).operation == Operations::FENCEI);
+    REQUIRE(decode_instruction(ilui).operation == Operations::LUI);
+    REQUIRE(decode_instruction(iauipc).operation == Operations::AUIPC);
+    REQUIRE(decode_instruction(ijal).operation == Operations::JAL);
+    REQUIRE(decode_instruction(ijalr).operation == Operations::JALR);
+    REQUIRE(decode_instruction(ibeq).operation == Operations::BEQ);
+    REQUIRE(decode_instruction(ibne).operation == Operations::BNE);
+    REQUIRE(decode_instruction(iblt).operation == Operations::BLT);
+    REQUIRE(decode_instruction(ibge).operation == Operations::BGE);
+    REQUIRE(decode_instruction(ibltu).operation == Operations::BLTU);
+    REQUIRE(decode_instruction(ibgeu).operation == Operations::BGEU);
+    REQUIRE(decode_instruction(ilb).operation == Operations::LB);
+    REQUIRE(decode_instruction(ilh).operation == Operations::LH);
+    REQUIRE(decode_instruction(ilw).operation == Operations::LW);
+    REQUIRE(decode_instruction(ilbu).operation == Operations::LBU);
+    REQUIRE(decode_instruction(ilhu).operation == Operations::LHU);
+    REQUIRE(decode_instruction(isb).operation == Operations::SB);
+    REQUIRE(decode_instruction(ish).operation == Operations::SH);
+    REQUIRE(decode_instruction(isw).operation == Operations::SW);
+    REQUIRE(decode_instruction(iaddi).operation == Operations::ADDI);
+    REQUIRE(decode_instruction(islti).operation == Operations::SLTI);
+    REQUIRE(decode_instruction(isltiu).operation == Operations::SLTIU);
+    REQUIRE(decode_instruction(ixori).operation == Operations::XORI);
+    REQUIRE(decode_instruction(iori).operation == Operations::ORI);
+    REQUIRE(decode_instruction(iandi).operation == Operations::ANDI);
+    REQUIRE(decode_instruction(islli).operation == Operations::SLLI);
+    REQUIRE(decode_instruction(isrli).operation == Operations::SRLI);
+    REQUIRE(decode_instruction(israi).operation == Operations::SRAI);
+    REQUIRE(decode_instruction(iadd).operation == Operations::ADD);
+    REQUIRE(decode_instruction(isub).operation == Operations::SUB);
+    REQUIRE(decode_instruction(isll).operation == Operations::SLL);
+    REQUIRE(decode_instruction(islt).operation == Operations::SLT);
+    REQUIRE(decode_instruction(isltu).operation == Operations::SLTU);
+    REQUIRE(decode_instruction(ixor).operation == Operations::XOR);
+    REQUIRE(decode_instruction(isrl).operation == Operations::SRL);
+    REQUIRE(decode_instruction(isra).operation == Operations::SRA);
+    REQUIRE(decode_instruction(ior).operation == Operations::OR);
+    REQUIRE(decode_instruction(iand).operation == Operations::AND);
+    REQUIRE(decode_instruction(ifence).operation == Operations::FENCE);
+    REQUIRE(decode_instruction(ifencei).operation == Operations::FENCEI);
 }
 
 TEST_CASE("R-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b00000000001000001000000110110011);
-	REQUIRE(a.funct7 == 0);
-	REQUIRE(a.funct3 == 0);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rs2 == 0b00010);
-	REQUIRE(a.rd ==  0b00011);
-	
-	a = decode_instruction(0b00001110001000001011000110110011);
-	REQUIRE(a.funct7 == 7);
-	REQUIRE(a.funct3 == 3);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rs2 == 0b00010);
-	REQUIRE(a.rd ==  0b00011);
+    auto a = decode_instruction(0b00000000001000001000000110110011);
+    REQUIRE(a.funct7 == 0);
+    REQUIRE(a.funct3 == 0);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rs2 == 0b00010);
+    REQUIRE(a.rd == 0b00011);
+
+    a = decode_instruction(0b00001110001000001011000110110011);
+    REQUIRE(a.funct7 == 7);
+    REQUIRE(a.funct3 == 3);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rs2 == 0b00010);
+    REQUIRE(a.rd == 0b00011);
 }
 
 TEST_CASE("I-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b00000000000100001000000010010011);
-	REQUIRE(a.funct3 == 0);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rd ==  0b00001);
-	REQUIRE(a.simm ==  1);
-	
-	a = decode_instruction(0b11111111111000001011000010010011);
-	REQUIRE(a.funct3 == 3);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rd ==  0b00001);
-	REQUIRE(a.simm ==  -2);
+    auto a = decode_instruction(0b00000000000100001000000010010011);
+    REQUIRE(a.funct3 == 0);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rd == 0b00001);
+    REQUIRE(a.simm == 1);
+
+    a = decode_instruction(0b11111111111000001011000010010011);
+    REQUIRE(a.funct3 == 3);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rd == 0b00001);
+    REQUIRE(a.simm == -2);
 }
 
 TEST_CASE("S-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b00000000000100001000000010100011);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rs2 == 0b00001);
-	REQUIRE(a.simm ==  1);
-	
-	a = decode_instruction(0b11111110111100001000111100100011);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rs2 == 0b01111);
-	REQUIRE(a.simm ==  -2);
+    auto a = decode_instruction(0b00000000000100001000000010100011);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rs2 == 0b00001);
+    REQUIRE(a.simm == 1);
+
+    a = decode_instruction(0b11111110111100001000111100100011);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rs2 == 0b01111);
+    REQUIRE(a.simm == -2);
 }
 
 TEST_CASE("B-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b00000000000100001001000101100011);
-	REQUIRE(a.rs1 == 0b00001);
-	REQUIRE(a.rs2 == 0b00001);
-	REQUIRE(a.simm ==  2);
-	
-	a = decode_instruction(0b11111110000111111001111111100011);
-	REQUIRE(a.rs1 == 0b11111);
-	REQUIRE(a.rs2 == 0b00001);
-	REQUIRE(a.simm ==  -2);
+    auto a = decode_instruction(0b00000000000100001001000101100011);
+    REQUIRE(a.rs1 == 0b00001);
+    REQUIRE(a.rs2 == 0b00001);
+    REQUIRE(a.simm == 2);
+
+    a = decode_instruction(0b11111110000111111001111111100011);
+    REQUIRE(a.rs1 == 0b11111);
+    REQUIRE(a.rs2 == 0b00001);
+    REQUIRE(a.simm == -2);
 }
 
 TEST_CASE("U-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b00000000000000000001000010110111);
-	REQUIRE(a.rd == 0b00001);
-	REQUIRE(a.simm ==  4096);
-	
-	a = decode_instruction(0b11111111111111111111111110010111);
-	REQUIRE(a.rd == 0b11111);
-	REQUIRE(a.simm ==  -4096);
+    auto a = decode_instruction(0b00000000000000000001000010110111);
+    REQUIRE(a.rd == 0b00001);
+    REQUIRE(a.simm == 4096);
+
+    a = decode_instruction(0b11111111111111111111111110010111);
+    REQUIRE(a.rd == 0b11111);
+    REQUIRE(a.simm == -4096);
 }
 
 TEST_CASE("J-Type Upacking", "[decoding]") {
-	auto a = decode_instruction(0b11111111100111111111000001101111);
-	REQUIRE(a.rd == 0b00000);
-	REQUIRE(a.simm ==  -8);
-	
-	a = decode_instruction(0b00000000100000000000111111101111);
-	REQUIRE(a.rd == 0b11111);
-	REQUIRE(a.simm ==  8);
+    auto a = decode_instruction(0b11111111100111111111000001101111);
+    REQUIRE(a.rd == 0b00000);
+    REQUIRE(a.simm == -8);
+
+    a = decode_instruction(0b00000000100000000000111111101111);
+    REQUIRE(a.rd == 0b11111);
+    REQUIRE(a.simm == 8);
 }
