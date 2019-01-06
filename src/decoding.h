867466bc6a768990100cdb49a476de26ff082bfd
Rohan Agrawal <send2arohan@gmail.com>
2019-01-05 23:13:23 -0800

    format instructions using custom fmt printer

--- src/decoding.h
+++ src/decoding.h
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <fmt/format.h>
 
 enum class Operations {
     LUI,
@@ -59,6 +60,103 @@ struct Instruction {
 };
 
 Instruction decode_instruction(uint32_t raw_inst);
+
+namespace fmt
+{
+template <>
+struct formatter<Instruction> {
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const Instruction& inst, FormatContext& ctx) {
+        switch (inst.operation) {
+            case Operations::LUI:
+                return format_to(ctx.begin(), "LUI x{} ← #{}", inst.rd, inst.imm);
+            case Operations::AUIPC:
+                return format_to(ctx.begin(), "AUIPC x{} ← #(pc + {})", inst.rd, inst.imm);
+            case Operations::JAL:
+                return format_to(ctx.begin(), "JAL x{}, #{}", inst.rd, inst.simm);
+            case Operations::JALR:
+                return format_to(ctx.begin(), "JALR x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BEQ:
+                return format_to(ctx.begin(), "BEQ x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BNE:
+                return format_to(ctx.begin(), "BNE x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BLT:
+                return format_to(ctx.begin(), "BLT x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BGE:
+                return format_to(ctx.begin(), "BGE x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BLTU:
+                return format_to(ctx.begin(), "BLTU x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::BGEU:
+                return format_to(ctx.begin(), "BGEU x{}, x{}, #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::LB:
+                return format_to(ctx.begin(), "LB x{} ← [x{} + {}]", inst.rd, inst.rs1, inst.simm);
+            case Operations::LH:
+                return format_to(ctx.begin(), "LH x{} ← [x{} + #{}]", inst.rd, inst.rs1, inst.simm);
+            case Operations::LW:
+                return format_to(ctx.begin(), "LW x{} ← [x{} + #{}]", inst.rd, inst.rs1, inst.simm);
+            case Operations::LBU:
+                return format_to(ctx.begin(), "LBU x{} ← [x{} + #{}]", inst.rd, inst.rs1, inst.simm);
+            case Operations::LHU:
+                return format_to(ctx.begin(), "LHU x{} ← [x{} + #{}]", inst.rd, inst.rs1, inst.simm);
+            case Operations::SB:
+                return format_to(ctx.begin(), "SB [x{} + #{}] ← x{}", inst.rs1, inst.simm, inst.rs2);
+            case Operations::SH:
+                return format_to(ctx.begin(), "SH [x{} + #{}] ← x{}", inst.rs1, inst.simm, inst.rs2);
+            case Operations::SW:
+                return format_to(ctx.begin(), "SW [x{} + #{}] ← x{}", inst.rs1, inst.simm, inst.rs2);
+            case Operations::ADDI:
+                return format_to(ctx.begin(), "ADDI x{} ← x{} + #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::SLTI:
+                return format_to(ctx.begin(), "SLTI x{} ← x{} < #{}", inst.rd, inst.rs1, inst.simm);
+            case Operations::SLTIU:
+                return format_to(ctx.begin(), "SLTIU x{} ← x{} < #{}", inst.rd, inst.rs1, inst.imm);
+            case Operations::XORI:
+                return format_to(ctx.begin(), "XORI x{} ← x{} ^ #{}", inst.rd, inst.rs1, inst.imm);
+            case Operations::ORI:
+                return format_to(ctx.begin(), "ORI x{} ← x{} | #{}", inst.rd, inst.rs1, inst.imm);
+            case Operations::ANDI:
+                return format_to(ctx.begin(), "ANDI x{} ← x{} & #{}", inst.rd, inst.rs1, inst.imm);
+            case Operations::SLLI:
+                return format_to(ctx.begin(), "SLLI");
+            case Operations::SRLI:
+                return format_to(ctx.begin(), "SRLI");
+            case Operations::SRAI:
+                return format_to(ctx.begin(), "SRAI");
+            case Operations::ADD:
+                return format_to(ctx.begin(), "ADD x{} ← x{} + x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SUB:
+                return format_to(ctx.begin(), "SUB x{} ← x{} - x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SLL:
+                return format_to(ctx.begin(), "SLL x{} ← x{} << x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SLT:
+                return format_to(ctx.begin(), "SLT x{} ← x{} < x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SLTU:
+                return format_to(ctx.begin(), "SLTU x{} ← x{} < x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::XOR:
+                return format_to(ctx.begin(), "XOR x{} ← x{} ^ x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SRL:
+                return format_to(ctx.begin(), "SRL x{} ← x{} >> x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::SRA:
+                return format_to(ctx.begin(), "SRA x{} ← x{} >> x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::OR:
+                return format_to(ctx.begin(), "OR x{} ← x{} | x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::AND:
+                return format_to(ctx.begin(), "AND x{} ← x{} & x{}", inst.rd, inst.rs1, inst.rs2);
+            case Operations::FENCE:
+                return format_to(ctx.begin(), "FENCE");
+            case Operations::FENCEI:
+                return format_to(ctx.begin(), "FENCEI");
+        }
+        return format_to(ctx.begin(), "Error formatting");
+    }
+};
+}  // namespace fmt
+
 
 /*** Inline utility functions ***/
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,10 @@
 #include <cstdio>
 #include <cstdint>
 #include <stdexcept>
-
+#include <fmt/format.h>
 #include <unistd.h>
+
 #include "decoding.h"
-
-void print_instruction(uint32_t raw_inst) {
-    auto inst = decode_instruction(raw_inst);
-
-    switch (inst.operation) {
-        case Operations::LUI:
-            printf("LUI x%d ← #%d\n", inst.rd, inst.imm);
-            break;
-        case Operations::AUIPC:
-            printf("AUIPC x%d ← #(pc + %d)\n", inst.rd, inst.imm);
-            break;
-        case Operations::JAL:
-            printf("JAL %d, %d\n", inst.rd, inst.simm);
-            break;
-        case Operations::JALR:
-            printf("JALR x%d, x%d, #%d\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::BEQ:
-            printf("BEQ x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::BNE:
-            printf("BNE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::BLT:
-            printf("BLT x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::BGE:
-            printf("BGE x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::BLTU:
-            printf("BLTU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::BGEU:
-            printf("BGEU x%d,x%d,%d\n", inst.rs1, inst.rs2, inst.simm);
-            break;
-        case Operations::LB:
-            printf("LB x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::LH:
-            printf("LH x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::LW:
-            printf("LW x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::LBU:
-            printf("LBU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::LHU:
-            printf("LHU x%d ← [x%d + %d]\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::SB:
-            printf("SB [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-            break;
-        case Operations::SH:
-            printf("SH [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-            break;
-        case Operations::SW:
-            printf("SW [x%d + #%d] ← x%d\n", inst.rs1, inst.simm, inst.rs2);
-            break;
-        case Operations::ADDI:
-            printf("ADDI x%d ← x%d + #%d\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::SLTI:
-            printf("SLTI x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.simm);
-            break;
-        case Operations::SLTIU:
-            printf("SLTIU x%d ← x%d < #%d\n", inst.rd, inst.rs1, inst.imm);
-            break;
-        case Operations::XORI:
-            printf("XORI x%d ← x%d ^ #%d\n", inst.rd, inst.rs1, inst.imm);
-            break;
-        case Operations::ORI:
-            printf("ORI x%d ← x%d | #%d\n", inst.rd, inst.rs1, inst.imm);
-            break;
-        case Operations::ANDI:
-            printf("ANDI x%d ← x%d & #%d\n", inst.rd, inst.rs1, inst.imm);
-            break;
-        case Operations::SLLI:
-            printf("SLLI\n");
-            break;
-        case Operations::SRLI:
-            printf("SRLI\n");
-            break;
-        case Operations::SRAI:
-            printf("SRAI\n");
-            break;
-        case Operations::ADD:
-            printf("ADD x%d ← x%d + x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SUB:
-            printf("SUB x%d ← x%d - x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SLL:
-            printf("SLL x%d ← x%d << x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SLT:
-            printf("SLT x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SLTU:
-            printf("SLTU x%d ← x%d < x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::XOR:
-            printf("XOR x%d ← x%d ^ x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SRL:
-            printf("SRL x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::SRA:
-            printf("SRA x%d ← x%d >> x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::OR:
-            printf("OR x%d ← x%d | x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::AND:
-            printf("AND x%d ← x%d & x%d\n", inst.rd, inst.rs1, inst.rs2);
-            break;
-        case Operations::FENCE:
-            printf("FENCE\n");
-            break;
-        case Operations::FENCEI:
-            printf("FENCEI\n");
-            break;
-    }
-}
 
 int32_t registers[32] = {};
 uint32_t program[10] = {
@@ -147,9 +24,9 @@ int main() {
 
     while (program_counter < END_PROGRAM) {
         const auto raw_inst = program[program_counter];
-        printf("-----------------------------------------\n");
-        print_instruction(raw_inst);
         const auto inst = decode_instruction(raw_inst);
+        puts("-----------------------------------------");
+        fmt::print("{}\n", inst);
 
         bool pc_set = false;
         switch (inst.operation) {
