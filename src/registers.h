d3f33533cba757848328030653f2c0bc1ee08ba6
Rohan Agrawal <send2arohan@gmail.com>
2019-01-08 22:31:46 -0800

    better comments in register header

--- src/registers.h
+++ src/registers.h
@@ -6,7 +6,11 @@ class Registers {
   public:
     Registers();
     uint32_t get(std::uint8_t reg) const;
-    const uint32_t& operator[](std::uint8_t reg) const; //operator[] is const only
+    // operator[] is const only to stop code like this r[3] = 6
+    // If allowed would bypass the 0 register check, wrecking havoc
+    const uint32_t& operator[](std::uint8_t reg) const;
+
+    // Return value of set will the the value written
     uint32_t set(std::uint8_t reg, uint32_t value);
   private:
     enum {n_registers = 32}; // Ugly hack to avoid linker errors
